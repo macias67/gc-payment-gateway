85e3c594e9ac0be3430be7cf773b7f82bb9e3428
Luis Macias <1738146+macias67@users.noreply.github.com>
2023-05-30 10:07:16 -0600

    Test Suite

---
+++ tests/unit/app/Services/MercadoPagoServiceTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace unit\app\Services;
+
+use App\Services\MercadoPagoService;
+use PHPUnit\Framework\TestCase;
+
+class MercadoPagoServiceTest extends TestCase
+{
+    private MercadoPagoService $mercadoPagoService;
+
+    private $mockSDK;
+
+    public function testGetCustomerReturnsCustomerObject(): void
+    {
+        $email = 'test@example.com';
+
+        $customer = $this->mercadoPagoService->getCustomer($email);
+
+        $this->assertInstanceOf(Customer::class, $customer);
+        $this->assertEquals($email, $customer->email);
+    }
+
+    public function testGetCustomerReturnsNullWhenCustomerNotFound()
+    {
+        $email = 'nonexistent@example.com';
+
+        $customer = $this->mercadoPagoService->getCustomer($email);
+
+        $this->assertNull($customer);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->mercadoPagoService = new MercadoPagoService();
+    }
+}
